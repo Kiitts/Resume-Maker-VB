6969daee988a2ec99d952d216d4bed561fd8ab15
Johndell S. Kitts <k.jaydeee@gmail.com>
2022-07-07 16:02:37 +0800

    enable checkbox will now enable the table for working experience if checked

--- Resume Maker VB/Form1.vb
+++ Resume Maker VB/Form1.vb
@@ -1,5 +1,9 @@
 ﻿Public Class Form1
-    Private Sub tableExperience_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles tableExperience.CellContentClick
-
+    Private Sub checkEnableWorkingExperience_CheckedChanged(sender As Object, e As EventArgs) Handles checkEnableWorkingExperience.CheckedChanged
+        If checkEnableWorkingExperience.Checked Then
+            tableExperience.Enabled = True
+        Else
+            tableExperience.Enabled = False
+        End If
     End Sub
 End Class
