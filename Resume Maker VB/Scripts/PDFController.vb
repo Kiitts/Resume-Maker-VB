b766484e1a7bdcfeb7a364710f998509b1e12f09
Johndell S. Kitts <k.jaydeee@gmail.com>
2022-07-07 23:11:16 +0800

    the pdf generate the educational attainment also

--- Resume Maker VB/Scripts/PDFController.vb
+++ Resume Maker VB/Scripts/PDFController.vb
@@ -15,7 +15,9 @@ Module PDFController
         ' Fonts
         Dim tNRoman As BaseFont = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, False)
         Dim nameFont As New Font(tNRoman, 20, Font.BOLD)
-        Dim normalFont As New Font(tNRoman, 12)
+        Dim subHeader As New Font(tNRoman, 12, Font.BOLD)
+        Dim normalFont As New Font(tNRoman, 11)
+        Dim tableHeader As New Font(tNRoman, 11, Font.BOLD)
 
         ' Texts
         ' Name
@@ -35,11 +37,34 @@ Module PDFController
                                               $"{userInfo.ContactNumber}" + vbCrLf +
                                               $"{userInfo.EmailAddress}", normalFont)
         contactParagraph.Alignment = Element.ALIGN_CENTER
+        ' Educational Attainment
+        Static educTable As PdfPTable = New PdfPTable(3)
+        If userInfo.EducationalAttainment.Count > 0 Then
+            educTable.WidthPercentage = 100
+            educTable.AddCell(New Paragraph("School", tableHeader))
+            educTable.AddCell(New Paragraph("Degree", tableHeader))
+            educTable.AddCell(New Paragraph("Year Attended", tableHeader))
+            For Each educDict As Dictionary(Of String, String) In userInfo.EducationalAttainment
+                educTable.AddCell(New Paragraph(educDict("School"), normalFont))
+                educTable.AddCell(New Paragraph(educDict("Degree"), normalFont))
+                educTable.AddCell(New Paragraph(educDict("Year Attended"), normalFont))
+            Next
+        End If
 
-
+        ' Creating resume PDF
         doc.Open()
         doc.Add(nameParagraph)
         doc.Add(contactParagraph)
+        doc.Add(New Paragraph(vbCrLf))
+        doc.Add(New draw.LineSeparator(1.0F, 100.0F, BaseColor.BLACK, Element.ALIGN_CENTER, 1))
+        doc.Add(New Paragraph("Objectives:", subHeader))
+        doc.Add(New Paragraph(userInfo.Summary, normalFont))
+        doc.Add(New Paragraph(vbCrLf))
+        If userInfo.EducationalAttainment.Count > 0 Then
+            doc.Add(New Paragraph("Educational Attainment:", subHeader))
+            doc.Add(New Paragraph(vbCrLf))
+            doc.Add(educTable)
+        End If
         doc.Close()
     End Sub
 End Module
